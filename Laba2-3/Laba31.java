6ebbe3df741577e8db6a8a1471d67e03aaf4c887
Leleka0 <tim.shiray@gmail.com>
2025-03-14 12:50:26 +0200

    Оновлення

--- Laba2-3/Laba31.java
+++ Laba2-3/Laba31.java
@@ -5,21 +5,22 @@ public class Laba31 {
         double T = 5.0;  
         double hx = 0.2;
 
-        boolean firstIteration = true;
-        double maxY = 0;
-        double maxX = 0;
+        double x = 0; 
+        double z = Math.pow(x, 4) + a * x + Math.pow(b, 3);
+        double maxY = Math.pow(Math.cos(z + a), 3) - x; 
+        double maxX = x;
 
-        for (double x = 0; x <= T; x += hx) {
-            double z = Math.pow(x, 4) + a * x + Math.pow(b, 3);
+        for (x += hx; x <= T; x += hx) { 
+            z = Math.pow(x, 4) + a * x + Math.pow(b, 3);
             double y = Math.pow(Math.cos(z + a), 3) - x;
 
-            if (firstIteration || y > maxY) {
+            if (y > maxY) {
                 maxY = y;
                 maxX = x;
-                firstIteration = false;
             }
         }
 
         System.out.println("Максимальне значення y: " + maxY + " при x = " + maxX);
     }
 }
+
