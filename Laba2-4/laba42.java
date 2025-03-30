b2a543d206d1106d379e344d0e242e30577e1cc7
Leleka0 <tim.shiray@gmail.com>
2025-03-30 18:38:23 +0300

    Laba42

--- Laba2-4/laba42.java
+++ Laba2-4/laba42.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
 
 public class laba42 {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = 10; 
+        double[] Arr = new double[num];
+        
+        
+        for (int i = 0; i < num; i++) {
+            Arr[i] = Math.random() * 20 - 10;
+        }
+        
+        
+        System.out.print("Масив: ");
+        for (double value : Arr) {
+            System.out.printf("%.2f ", value);
+        }
+        System.out.println();
+        
+       
+        int count = 0;
+        for (int i = 1; i < num - 1; i++) {
+            if (Arr[i] > Arr[i - 1] && Arr[i] > Arr[i + 1]) {
+                count++;
+            }
+        }
+        
+        
+        System.out.println("Кількість елементів, більших за сусідів: " + count);
+        
+        in.close();
+    }
 }
