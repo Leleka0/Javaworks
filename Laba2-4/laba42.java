8f90b3c614ff3f62fa3f233ca44f5be3baa28224
Leleka0 <tim.shiray@gmail.com>
2025-04-02 12:08:08 +0300

    Редагування laba42

--- Laba2-4/laba42.java
+++ Laba2-4/laba42.java
@@ -4,21 +4,21 @@ public class laba42 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int num = 10; 
-        double[] Arr = new double[num];
+        int[] Arr = new int[num]; 
         
         
         for (int i = 0; i < num; i++) {
-            Arr[i] = Math.random() * 20 - 10;
+            Arr[i] = (int) (Math.random() * 20) - 6;
         }
         
-        
+       
         System.out.print("Масив: ");
-        for (double value : Arr) {
-            System.out.printf("%.2f ", value);
+        for (int value : Arr) {
+            System.out.print(value + " ");
         }
         System.out.println();
         
-       
+        
         int count = 0;
         for (int i = 1; i < num - 1; i++) {
             if (Arr[i] > Arr[i - 1] && Arr[i] > Arr[i + 1]) {
