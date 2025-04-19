be5e8fbb6322aa3355063192283e841154dfb7df
Leleka0 <tim.shiray@gmail.com>
2025-04-19 13:44:04 +0300

    Редагування коду

---
+++ Laba2-5/laba5.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class laba5 {
+    public static void main(String[] args) {
+        int n = 4, m = 5;
+        int[][] N = new int[n][m];
+        int[] P = new int[m]; 
+
+        
+        System.out.println("Масив N:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                N[i][j] = (int)(Math.random() * 100); 
+                System.out.print(N[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        
+        for (int j = 0; j < m; j++) {
+            P[j] = 1;
+            boolean found = false;
+            for (int i = 0; i < n; i++) {
+                if (N[i][j] >= 10 && N[i][j] < 55) {
+                    P[j] *= N[i][j];
+                    found = true;
+                }
+            }
+            if (!found) P[j] = 0; 
+        }
+
+        
+        System.out.println("\nВектор добутків по стовпцях:");
+        for (int j = 0; j < m; j++) {
+            System.out.print(P[j] + "\t");
+        }
+    }
+}
