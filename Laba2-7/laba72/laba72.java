b96fd20591a8a2f0ce3d0dbe65e01beddf2aa554
Leleka0 <tim.shiray@gmail.com>
2025-05-20 12:52:44 +0300

    Ремонт Лаба72

--- Laba2-7/laba72/laba72.java
+++ Laba2-7/laba72/laba72.java
@@ -1,12 +1,6 @@
-package laba72;
-
 //Батьківський клас, що представляє академгрупу з методами виведення на екран назви групи і числа студентів в ній. Дочірній клас
 //який представляє потік (об'єднання) декількох академгруп з методами, які виводять назву потоку і загальне число студентів в потоці
 //Головний клас з методом main, який одним оператором виводить назву і кількість студентів в академгрупі або потоці, які є елементами загального масиву
-// Базовий клас — академгрупа
-
-
-
 class AcademGroup {
     protected String groupName;
     protected int studentCount;
@@ -16,50 +10,53 @@ class AcademGroup {
         this.studentCount = studentCount;
     }
 
-    public int getStudentCount() {
-        return studentCount;
-    }
-
+    
     public void printInfo() {
-        System.out.println("Академгрупа: " + groupName + ", Студентiв: " + studentCount);
+        System.out.println("Академгрупа: " + groupName + ", Студентів: " + studentCount);
     }
 }
+
 
 class Stream extends AcademGroup {
     private String streamName;
     private AcademGroup[] groups;
 
     public Stream(String streamName, AcademGroup[] groups) {
-        super(streamName, 0);
+        super(streamName, 0); 
         this.streamName = streamName;
         this.groups = groups;
-
+        
         int total = 0;
         for (AcademGroup g : groups) {
-            total += g.getStudentCount();
+            total += g.studentCount;
         }
         this.studentCount = total;
     }
 
+    
     @Override
     public void printInfo() {
-        System.out.println("Потiк: " + streamName + ", Загальна кiлькiсть студентiв: " + studentCount);
+        System.out.println("Потік: " + streamName + ", Загальна кількість студентів: " + studentCount);
     }
 }
 
+
 public class laba72 {
     public static void main(String[] args) {
+      
         AcademGroup g1 = new AcademGroup("ТТ-201", 16);
         AcademGroup g2 = new AcademGroup("ТТ-202", 19);
         AcademGroup g3 = new AcademGroup("ТТ-203", 20);
 
+       
         Stream s1 = new Stream("ТТ", new AcademGroup[]{g1, g2, g3});
 
+       
         AcademGroup[] elements = {g1, g2, g3, s1};
 
+       
         for (AcademGroup ag : elements) {
-            ag.printInfo();
+            ag.printInfo(); 
         }
     }
 }
-
